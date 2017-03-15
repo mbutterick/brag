794dcbd5418859709d4c8c1adf2266a07e293c9c
Matthew Butterick <mb@mbtype.com>
2017-03-15 10:27:44 -0700

    move version number

--- brag/brag/info.rkt
+++ brag/brag/info.rkt
@@ -1,6 +1,6 @@
 #lang setup/infotab
 (define name "brag")
-(define version "1.0")
+
 (define scribblings '(("brag.scrbl")))
 (define blurb '("brag: the Beautiful Racket AST Generator. A fork of Danny Yoo's ragg. A design goal is to be easy for beginners to use. Given a grammar in EBNF, brag produces a parser that generates Racket's native syntax objects with full source location."))
 (define deps (list))
--- brag/info.rkt
+++ brag/info.rkt
@@ -1,5 +1,6 @@
 #lang setup/infotab
 
+(define version "1.0")
 (define deps '("base" "br-parser-tools-lib" "rackunit-lib"))
 (define build-deps '("at-exp-lib" "br-parser-tools-doc" "racket-doc"
                      "scribble-lib"))
