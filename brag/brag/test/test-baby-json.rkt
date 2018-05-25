867b84f16e69722d216ead4d920a2890b7081fcf
Matthew Butterick <mb@mbtype.com>
2018-05-24 21:05:41 -0700

    test new syntax

--- brag/brag/test/test-baby-json.rkt
+++ brag/brag/test/test-baby-json.rkt
@@ -1,33 +1,30 @@
 #lang racket/base
 (require brag/examples/baby-json
+         (prefix-in alt: brag/examples/baby-json-alt)
          brag/support
          rackunit)
 
-(check-equal?
- (syntax->datum
-  (parse (list "{" 
-               (token 'ID "message")
-               ":"
-               (token 'STRING "'hello world'")
-               "}")))
- '(json (object "{"
-                (kvpair "message" ":" (json (string "'hello world'")))
-                "}")))
+(let ([str (list "{" 
+                 (token 'ID "message")
+                 ":"
+                 (token 'STRING "'hello world'")
+                 "}")]
+      [result '(json (object "{"
+                             (kvpair "message" ":" (json (string "'hello world'")))
+                             "}"))])
+  (check-equal? (parse-to-datum str) result)
+  (check-equal? (alt:parse-to-datum str) result))
 
 
-(check-equal? 
- (syntax->datum
-  (parse "[[[{}]],[],[[{}]]]"))
- '(json
-   (array
-    "["
-    (json (array "[" (json (array "[" (json (object "{" "}")) "]")) "]"))
-    ","
-    (json (array "[" "]"))
-    ","
-    (json (array "[" (json (array "[" (json (object "{" "}")) "]")) "]"))
-    "]")))
-
- 
-               
-               
+(let ([str "[[[{}]],[],[[{}]]]"]
+      [result  '(json
+                 (array
+                  "["
+                  (json (array "[" (json (array "[" (json (object "{" "}")) "]")) "]"))
+                  ","
+                  (json (array "[" "]"))
+                  ","
+                  (json (array "[" (json (array "[" (json (object "{" "}")) "]")) "]"))
+                  "]"))])
+  (check-equal? (parse-to-datum str) result)
+  (check-equal? (alt:parse-to-datum str) result))
